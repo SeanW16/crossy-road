1e90e64a320340ed3b42d733de6a406e03d3fea5
seanwood453 <seanwood453@SDS-L-FGHDF63>
2024-06-10 14:01:42 -0400

    Add project files.

---
+++ crossy road/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace crossy_road
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.logoLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.startLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // logoLabel
+            // 
+            this.logoLabel.Image = global::crossy_road.Properties.Resources.images_removebg_preview__1_;
+            this.logoLabel.Location = new System.Drawing.Point(12, 38);
+            this.logoLabel.Name = "logoLabel";
+            this.logoLabel.Size = new System.Drawing.Size(368, 386);
+            this.logoLabel.TabIndex = 1;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // startLabel
+            // 
+            this.startLabel.Image = global::crossy_road.Properties.Resources.images_removebg_preview;
+            this.startLabel.Location = new System.Drawing.Point(16, 424);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(368, 119);
+            this.startLabel.TabIndex = 2;
+            this.startLabel.Click += new System.EventHandler(this.startLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(400, 700);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.logoLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label logoLabel;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label startLabel;
+    }
+}
+
---
+++ crossy road/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crossy_road
+{
+    public partial class Form1 : Form
+    {
+        //All shapes
+        Rectangle road1 = new Rectangle(0, 20, 400, 50);
+        Rectangle road2 = new Rectangle(0, 120, 400, 50);
+        Rectangle road3 = new Rectangle(0, 200, 400, 50);
+        Rectangle road4 = new Rectangle(0, 300, 400, 50);
+        Rectangle road5 = new Rectangle(0, 380, 400, 50);
+        Rectangle player = new Rectangle(200, 575, 10, 20);
+      
+
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+        //All bools
+        bool wPressed = false;
+        bool aPressed = false;
+        bool dPressed = false;
+
+        int playerSpeed = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void InitializeGame()
+        {
+            //reset everything for start of game
+            logoLabel.Image = null;
+            startLabel.Image = null;
+            logoLabel.BackColor = Color.Transparent;
+            startLabel.BackColor = Color.Transparent;
+
+            gameTimer.Enabled = true;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Key controls
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = true;
+                    break;
+                case Keys.A:
+                    aPressed = true;
+                    break;
+                case Keys.D:
+                    dPressed = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Key controls 
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wPressed = false;
+                    break;
+                case Keys.A:
+                    aPressed = false;
+                    break;
+                case Keys.D:
+                    dPressed = false;
+                    break;
+              
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameTimer.Enabled == true)
+            {
+                //draw shapes
+                e.Graphics.FillRectangle(whiteBrush, player);
+                e.Graphics.FillRectangle(blackBrush, road1);
+                e.Graphics.FillRectangle(blackBrush, road2);
+                e.Graphics.FillRectangle(blackBrush, road3);
+                e.Graphics.FillRectangle(blackBrush, road4);
+                e.Graphics.FillRectangle(blackBrush, road5);
+
+            }
+
+        }
+
+        private void startLabel_Click(object sender, EventArgs e)
+        {
+           
+            
+                InitializeGame();
+            
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move player 1
+            if (wPressed == true)
+            {
+                road1.Y = road1.Y + 10;
+                road2.Y = road2.Y + 10;
+                road3.Y = road3.Y + 10;
+                road4.Y = road4.Y + 10;
+                road5.Y = road5.Y + 10;
+            }
+            if (aPressed == true)
+            {
+                player.X = player.X - playerSpeed;
+
+              
+            }
+            if (dPressed == true)
+            {
+                player.X = player.X + playerSpeed;
+
+            
+            }
+            Refresh();
+
+        }
+    }
+}
